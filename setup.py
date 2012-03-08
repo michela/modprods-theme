4a8838d770425b5ec06c8f6e9b4162991dab402d
Lee Matos <find.creativity@leematos.com>
2012-03-08 17:07:04 -0500

    Make a module!

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="modprods_theme",
+	version="0.1",
+	description="Mod Prods theme for Mezzanine",
+    author="Lee Matos",
+	author_email="find.creativity@leematos.com",
+	maintainer="Lee Matos",
+	maintainer_email="find.creativity@leematos.com",
+	url="",
+	packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
